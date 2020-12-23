a04e1dbd508eb4d23130e0090e9fab22ed804498
feng <dongyfeng@aliyun.com>
2020-12-23 12:14:49 +0800

    buffer learn

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # netty-learn
 netty 学习
+
+## NIO 
+
+
+### Selector Channel Buffer 的关系：  
+1. channel 和 buffer 一对一
+2. Selector 对应一个线程，并对应多个 channel， 多个 channel 注册到 一个 selector 上
+3. 程序切换到哪个 channel 上是事件决定的，**event**  很重要
+4. Selector 根据不同的事件，在各个 channel 上切换
+5. buffer 就是一个内存块，底层是数组，和 channel 一一对应
+6. 数据的读写是通过 buffer 来完成的。BIO 要么是分为输入流和输出流，不能双向， NIO的 buffer 双向，但需要 flip 方法进行切换。
+7. channel 也是双向的，可以反映底层操作系统的情况，比如 ; linux , 底层操作系统就是双向的
+
+### buffer
+本质上就是一个可以读写数据的内存块，可以理解为容器对象（含数组），该对象提供了一组方法，可以轻松的使用内存块， buffer 对象内置了一些机制，能够跟踪记录缓冲区的状态变化， channel 提供从文件，网络读取数据的渠道，但是读写数据都是由 buffer 完成。  
+
+
+
--- src/main/java/com/yf/nio/BasicBuffer.java
+++ src/main/java/com/yf/nio/BasicBuffer.java
@@ -4,22 +4,36 @@ import java.nio.IntBuffer;
 
 /**
  * buffer 的使用
+ *
+ * 八种基本数据类型，除了 boolean ，都有对应的 buffer
+ * byteBuffer 最为重要
  */
 public class BasicBuffer {
 
     public static void main(String[] args) {
 
-        // 创建 buffer , 可以存放 5 个
-        IntBuffer intBuffer = IntBuffer.allocate(5);
+        /**
+         * private int mark = -1;
+         * private int position = 0;
+         * private int limit;
+         * private int capacity;
+         *     buffer 提供上面四个属性
+         */
+
+        // 创建 buffer , 可以存放 5 个 ， capacity 是 5
+        IntBuffer intBuffer = IntBuffer.allocate(5);  // position 置 0 ， limit = 5 类似 数组的 size 属性 最大索引加一,
 
         intBuffer.put(10);
         intBuffer.put(11);
         intBuffer.put(12);
         intBuffer.put(13);
-        intBuffer.put(14);
+        intBuffer.put(14);  // position 类比 动态维护的 size , 此处 position = 5 == limit
 
         // 从 buffer 读取数据
         intBuffer.flip();//todo 将 buffer 转换，读写切换
+
+        intBuffer.position(1); // todo 可以设置开始读的位置，表示底层从 数组的第二个元素开始读  即 11 12 13 14
+        intBuffer.limit(3); // todo  限制读到的位置，理解为数组容量，和 数组实际存放的数据，并设置limit 开始，之后的数据无法访问。即 11 12
         while (intBuffer.hasRemaining()) {
             System.out.println(intBuffer.get());
         }
